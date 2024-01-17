9c86b52aa0774fbcf3e9c4e133d9fe9ee60cd55e
Ann Catherine Jose <ann.jose@gmail.com>
2024-01-16 17:09:35 -0800

    Add a logo image and display it in the Banner component. Use CSS styles from bootstrap and the custom globals.css file.

--- app-plural-globomantics/components/banner.js
+++ app-plural-globomantics/components/banner.js
@@ -1,7 +1,10 @@
 const Banner = () => {
     return (
-        <header>
-            <div>Banner text</div>
+        <header className="row mb-4">
+            <div className="col-5">
+                <img src="/logo-globomantics.png" alt="Globomantics Logo" className="logo"/>
+            </div>
+            <div className="col-7 mt-5">Provides houses all over the word at affordable prices</div>
         </header>
     );
 }
