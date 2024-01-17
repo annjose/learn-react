40ac1ef552b7d146447bfed0c76535cbf3241a61
Ann Catherine Jose <ann.jose@gmail.com>
2024-01-16 17:28:05 -0800

    Use style attribute of the component - with inline style and pre-defined variable.

--- app-plural-globomantics/components/banner.js
+++ app-plural-globomantics/components/banner.js
@@ -1,10 +1,13 @@
 import styles from "./banner.module.css";
 
 const Banner = () => {
+    const logo = { height: "150px" };
+
     return (
         <header className="row mb-4">
             <div className="col-5">
-                <img src="/logo-globomantics.png" alt="Globomantics Logo" className={styles.logo}/>
+                {/* <img src="/logo-globomantics.png" alt="Globomantics Logo" style={logo} /> */}
+                <img src="/logo-globomantics.png" alt="Globomantics Logo" style={{ height: "150px" }} />
             </div>
             <div className="col-7 mt-5">Provides houses all over the word at affordable prices</div>
         </header>
