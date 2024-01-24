1146768adeeff8f95b7d0fc71344bad15afbdc83
Ann Catherine Jose <ann.jose@gmail.com>
2024-01-24 14:47:39 -0800

    Added player navigation with Play, Pause, Next, Prev to seek to markers on the cast. Also added listeners for marker events.

--- src/pages/helix-tutor.js
+++ src/pages/helix-tutor.js
@@ -6,57 +6,113 @@ import 'asciinema-player/dist/bundle/asciinema-player.css';
 const HelixTutor = () => {
 
     const challenges = [
-        { number: 1, title: 'Change all occurences of "helix" to uppercase, except for URLs', castFile: '/ch-01-solution.cast' }
+        { number: 1, title: 'Change all occurences of "helix" to uppercase, except for URLs', castFile: '/ch-01-solution.cast', markerCount: 6 }
     ];
 
-    const playerIsLoaded = useRef(false);
+    const player = useRef(null);
 
     const [keystrokes, setKeystrokes] = useState([]);
 
     const currentChallenge = challenges[0];
 
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentMarkerIndex, setCurrentMarkerIndex] = useState(-1);
+
     useEffect(() => {
 
-        if (playerIsLoaded.current) {
+        if (player.current) {
             console.log('useEffect called after loading player. Exiting useEffect now.')
             return;
         }
         const chPlayerElement = document.getElementById('ch-player');
         const options = {};
-        const player = AsciinemaPlayer.create(currentChallenge.castFile, chPlayerElement, options);
 
-        player.addEventListener('play', () => {
+        player.current = AsciinemaPlayer.create(currentChallenge.castFile, chPlayerElement, options);
+
+        player.current.addEventListener('play', () => {
             console.log('play!');
+            setIsPlaying(true);
         })
 
-        player.addEventListener('pause', () => {
+        player.current.addEventListener('pause', () => {
             console.log("paused!");
+            setIsPlaying(false);
         })
 
-        player.addEventListener('playing', () => {
+        player.current.addEventListener('playing', () => {
             console.log(`playing!`);
         })
 
-        player.addEventListener('input', ({ data }) => {
-            const newInput = JSON.stringify(data);
-            // console.log('new input!', newInput);
+        player.current.addEventListener('ended', () => {
+            console.log("ended!");
+            setIsPlaying(false);
+        })
 
+        player.current.addEventListener('marker', marker => {
+            console.log(`reached marker. index: ${marker.index}, label: ${marker.label}`);
+            setCurrentMarkerIndex(marker.index);
+        })
+
+        player.current.addEventListener('input', ({ data }) => {
+            const newInput = JSON.stringify(data);
             setKeystrokes((prevState) => {
-                // console.log(`prevState=${prevState}`);
                 return [...prevState, newInput];
             })
         })
-
-        playerIsLoaded.current = true;
     }, []);
+
+    const PlayerNav = () => {
+
+        const handlePlayPauseButtonClick = () => {
+            if (isPlaying == true) {
+                player.current.pause();
+                setIsPlaying(false);
+            } else {
+                player.current.play();
+                setIsPlaying(true);
+            }
+        }
+
+        const handleMarkerButtonClick = (direction) => {
+            const markerIndex = direction == 'next' ? currentMarkerIndex + 1 : currentMarkerIndex - 1;
+            console.log(`seeing to marker index ${markerIndex}, current marker index: ${currentMarkerIndex}`);
+
+            player.current.seek({ marker: markerIndex });
+            setCurrentMarkerIndex(markerIndex);
+        }
+
+        const handleResetButton = () => {
+            setKeystrokes([]);
+        }
+
+        return (
+            <>
+                <div style={{ marginBottom: '24px' }}>
+                    <button style={{ marginRight: '40px' }} onClick={handlePlayPauseButtonClick}>
+                        {isPlaying == true ? 'Pause' : 'Play'}
+                    </button>
+
+                    <button
+                        style={{ marginRight: '8px' }}
+                        disabled={currentMarkerIndex <= 0 ? true : false}
+                        onClick={() => { handleMarkerButtonClick('prev') }}>Prev Marker</button>
+                    <button style={{ marginRight: '48px' }}
+                        disabled={currentMarkerIndex >= currentChallenge.markerCount - 1 ? true : false}
+                        onClick={() => { handleMarkerButtonClick('next') }}>Next Marker</button>
+
+                    <button onClick={handleResetButton}>Reset Data</button>
+                </div>
+            </>
+        )
+    };
 
     const KeystrokesPanel = () => {
         return (
             <>
                 <h3>Keystrokes</h3>
                 <ul>
-                    {keystrokes.map((keystroke) => (
-                        <li>{keystroke.replace(/\"/g, "")}</li>
+                    {keystrokes.map((keystroke, index) => (
+                        <span style={{marginRight: '8px'}}>{keystroke.replace(/\"/g, "")}</span>
                     ))}
                 </ul>
             </>
@@ -77,11 +133,11 @@ const HelixTutor = () => {
     const Commands = () => {
         let commands = [];
         let tempCommand = '';
+
         keystrokes.map((keystroke) => {
             keystroke = keystroke.replace(/\"/g, "");
-            // if (keystroke == '\"\\r\"') {
             if (keystroke == '\\r') {
-                console.log(`found command: ${tempCommand}`);
+                // console.log(`found command: ${tempCommand}`);
                 commands.push(tempCommand);
                 tempCommand = '';
             } else {
@@ -93,8 +149,8 @@ const HelixTutor = () => {
             <>
                 <h3>Commands</h3>
                 <ul>
-                    {commands.map((command) => (
-                        <li><pre>{command}</pre></li>
+                    {commands.map((command, index) => (
+                        <li key={index}><pre>{command}</pre></li>
                     ))}
                 </ul>
             </>
@@ -111,8 +167,9 @@ const HelixTutor = () => {
 
             <div id='ch-player' style={{ width: '928px', height: '670px' }}></div>
 
-            <Commands />
+            <PlayerNav />
             <KeystrokesPanel />
+            <Commands />
         </>
     );
 };
